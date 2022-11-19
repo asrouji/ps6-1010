5a840da35ebe622c461bc383671a04c0d87dce73
Aidan Srouji <72050533+asrouji@users.noreply.github.com>
2022-11-18 19:17:42 -0800

    add HighRollerGame class and updated tests

--- highroller.py
+++ highroller.py
@@ -1,158 +1,175 @@
 """
-Name:
-Date:
-Description:
+Name: Student
+Date: 11/18/22
+Description: High Roller
 """
 
 from random import randint
-
-"""
-####### Die Class: ######
-A simple class for representing die objects. A die has a given number of
-sides (at least) four, set when the die is constructed and which can never
-be changed. The die's value can be changed, but only by calling its roll()
-method.
-"""
+from sys import exit
 
 
 class Die():
-
     """
-    Constructs a die with the given starting value and number of sides.
+    A simple class for representing die objects. A die has a given number of
+    sides (at least) four, set when the die is constructed and which can never
+    be changed. The die's value can be changed, but only by calling its roll()
+    method.
     """
 
     def __init__(self, initial_value, sides):
+        """
+        Constructs a new Die object with two attributes:
+        - self.value: starting value of the die, given by parameter initial_value
+        - self.sides: number of sides of the die, given by parameter sides
+        """
         self.value = initial_value
         self.sides = sides
 
-    """
-    Simulates a roll by randomly updating the value of this die. 
-    In addition to mutating the die's value, this method also 
-    returns the new updated value.
-    """
-
     def roll(self):
+        """
+        Simulates a roll by randomly updating the value of this die. 
+        In addition to mutating the die's value, this method also 
+        returns the new updated value.
+        """
         self.value = randint(1, self.sides)
 
-    # Returns the number of sides of this die.
     def get_number_sides(self):
+        """
+        Returns the number of sides of this die.
+        """
         return self.sides
 
-    # Returns the current value of this die.
     def get_current_value(self):
+        """
+        Returns the current value of this die.
+        """
         return self.value
 
-    """
-    Returns a string representation of this die, which is its value enclosed in square
-    brackets, without spaces, for example "[5]".
-    """
-
     def __str__(self):
+        """
+        Returns a string representation of this die, which is its value enclosed in square
+        brackets, without spaces, for example "[5]".
+        """
         return "[{}]".format(self.value)
 
 
-"""
-####### DiceSet Class: ######
-A dice set holds a collection of Die objects. All of the die objects have
-the same number of sides. 
-"""
-
-
 class DiceSet():
-
     """
-    Creates a DiceSet containing the given number of dice, each with the 
-    given number of sides. All die values start off as 1.
+    A dice set holds a collection of Die objects. All of the die objects have
+    the same number of sides. 
     """
 
     def __init__(self, sides_each_die, number_of_dice):
+        """
+        Creates a new DiceSet. This constructor should initialize the following attributes:
+        - self.dice: a list of Die objects, each with the given number of sides
+        """
         self.dice = [Die(1, sides_each_die) for _ in range(number_of_dice)]
 
-    """
-    Returns the descriptor of the dice set; for example "5d20" for a set with
-    five dice of 20 sides each; or "2d6" for a set of two six-sided dice.
-    """
-
     def get_descriptor(self):
+        """
+        Returns the descriptor of the dice set; for example "5d20" for a set with
+        five dice of 20 sides each; or "2d6" for a set of two six-sided dice.
+        """
         return str(len(self.dice)) + "d" + str(self.dice[0].get_number_sides())
 
     # Returns the total of the values of each die in the set.
     def get_total(self):
+        """
+        Returns the total of the values of each die in the set.
+        """
         return sum([die.get_current_value() for die in self.dice])
 
-    # Rolls all the dice in the set.
     def roll_all(self):
-        for die in self.dice:
-            die.roll()
+        """
+        Rolls all of the dice in the set.
+        """
+        [die.roll() for die in self.dice]
 
-    # Rolls the i-th die, updating its value.
     def roll_die(self, i):
+        """
+        Rolls the die at the given index in the set.
+        """
         self.dice[i].roll()
 
-    # Returns the values of each of the dice in a list.
     def get_current_values(self):
+        """
+        Returns a list of the current values of each die in the set.
+        """
         return [die.get_current_value() for die in self.dice]
 
-    """
-    Returns whether this dice set has the same distribution of values as
-    another dice set. The two dice sets must have the same number of dice
-    and the same number of sides per dice, and there must be the same
-    number of each value in each set.
-    """
-
     def matches(self, dice_set):
+        """
+        Returns whether this dice set has the same distribution of values as
+        another dice set. The two dice sets must have the same number of dice
+        and the same number of sides per dice, and there must be the same
+        number of each value in each set.
+        """
         return self.get_descriptor() == dice_set.get_descriptor() and sorted(self.get_current_values()) == sorted(dice_set.get_current_values())
 
-    """
-    Returns a string representation in which each of the die strings are
-    joined without a separator, for example: "[2][5][2][3]".
-    """
-
     def __str__(self):
+        """
+        Returns a string representation in which each of the die strings are
+        joined without a separator, for example: "[2][5][2][3]".
+        """
         return "".join([str(die) for die in self.dice])
 
 
-def main():
-    """ 
-    ####### Main Program: ######
-    The program should begin by printing a welcome message. 
-    Then it repeatedly asks the user to enter a command and carries
-    it out. Each command will either print a response or an error 
-    message, followed by a blank line.
+class HighRollerGame():
     """
-    print("Welcome to HighRollerGame!")
+    A HighRollerGame object represents a single game of High Roller.
+    It maintains the current dice set and (optionally) the highest total so far.
+    """
 
-    dice = DiceSet(6, 5)
-    highest = dice.get_total()
-    print(dice)
+    def __init__(self):
+        """
+        Creates a new HighRollerGame. This constructor should initialize the following attributes:
+        - self.dice_set: a DiceSet containing 5 dice with 6 sides each
+        - (Optional) self.highest_total: the highest dice total so far, initialized as the total of the initial dice set
+        """
+        self.dice_set = DiceSet(6, 5)
+        self.highest_total = self.dice_set.get_total()
 
-    while True:
-        player_input = input("Enter a command: ")
-        if player_input == "":
-            player_input = "help"
-        command, *args = player_input.split()
-        if command == "roll":
-            if args[0] == "all":
-                dice.roll_all()
-            else:
-                dice.roll_die(int(args[0]))
-            print(dice)
-            highest = max(highest, dice.get_total())
-        elif command == "use":
-            dice = DiceSet(int(args[0]), int(args[1]))
-            highest = max(highest, dice.get_total())
-            print(dice.get_descriptor())
-            print(dice)
-        elif command == "highest" or command == "high":
-            print(highest)
-        elif command == "h" or command == "help":
-            print("Commands: roll all, roll i, h, help, q, quit")
-        elif command == "q" or command == "quit":
-            print("Thanks for playing!")
-            break
-        else:
-            print("Invalid command, use h or help for help!")
+    def run_command(self, command, args=[]):
+        """
+        Called after the player enters a command. This method accepts two arguments:
+        - command: the first word of the given command (e.g. "roll", "use", "help", etc.)
+        - args: list of command arguments (e.g. ["5", "4"] for "use 5 4" or ["all"] for "roll all")
+        """
+        match command:
+            case "roll":
+                if args[0] == "all":
+                    self.dice_set.roll_all()
+                else:
+                    self.dice_set.roll_die(int(args[0]))
+                print(self.dice_set)
+            case "use":
+                self.dice_set = DiceSet(int(args[0]), int(args[1]))
+                print(self.dice_set.get_descriptor())
+                print(self.dice_set)
+            case "highest":
+                print(f'Highest score so far: {self.highest_total}')
+            case "help":
+                print(
+                    "Commands: roll all, roll <die>, use <sides> <count>, highest, help, quit")
+            case "quit":
+                print("Goodbye!")
+                exit()
+            case _:
+                print("Unknown command")
+        if self.dice_set.get_total() > self.highest_total:
+            self.highest_total = self.dice_set.get_total()
+
+    def play(self):
+        """
+        Begins the game by repeatedly prompting the player for commands.
+        This method has been implemented for you; you should not need to modify it.
+        """
+        print("Welcome to High Roller Game!")
+        while True:
+            command, *args = input("Enter a command: ").split()
+            self.run_command(command, args)
 
 
 if __name__ == "__main__":
-    main()
+    HighRollerGame().play()
--- highroller_test.py
+++ highroller_test.py
@@ -1,5 +1,6 @@
+import random
 import unittest
-from highroller import Die, DiceSet
+from highroller import Die, DiceSet, HighRollerGame
 
 
 class DieTests(unittest.TestCase):
@@ -12,12 +13,16 @@ class DieTests(unittest.TestCase):
     def test_die_roll(self):
         die = Die(1, 6)
         die.roll()
-        self.assertTrue(1 <= die.get_current_value() <= 6)
+        self.assertTrue(1 <= die.value <= 6)
 
     def test_die_getters(self):
         die = Die(5, 6)
         self.assertEqual(die.get_number_sides(), 6)
         self.assertEqual(die.get_current_value(), 5)
+
+        die = Die(1, 20)
+        self.assertEqual(die.get_number_sides(), 20)
+        self.assertEqual(die.get_current_value(), 1)
 
     def test_die_str(self):
         die = Die(5, 6)
@@ -50,15 +55,16 @@ class DiceSetTests(unittest.TestCase):
     def test_dice_set_roll_all(self):
         dice_set = DiceSet(6, 5)
         dice_set.roll_all()
-        self.assertTrue(5 <= dice_set.get_total() <= 30)
+        self.assertEqual(dice_set.get_total(), sum(
+            [die.value for die in dice_set.dice]))
 
     def test_dice_set_roll_die(self):
         dice_set = DiceSet(6, 5)
-        dice_values = [die.get_current_value() for die in dice_set.dice]
+        old_dice_values = [die.get_current_value() for die in dice_set.dice]
         dice_set.roll_die(0)
         for i in range(1, 5):
             self.assertEqual(
-                dice_values[i], dice_set.dice[i].get_current_value())
+                old_dice_values[i], dice_set.dice[i].value)
 
     def test_dice_set_get_values(self):
         dice_set = DiceSet(6, 5)
@@ -70,23 +76,93 @@ class DiceSetTests(unittest.TestCase):
         self.assertEqual(str(dice_set), "[1][1][1][1][1]")
         dice_set.roll_all()
         self.assertEqual(str(dice_set), "[{}][{}][{}][{}][{}]".format(
-            dice_set.dice[0].get_current_value(),
-            dice_set.dice[1].get_current_value(),
-            dice_set.dice[2].get_current_value(),
-            dice_set.dice[3].get_current_value(),
-            dice_set.dice[4].get_current_value(),
+            dice_set.dice[0].value,
+            dice_set.dice[1].value,
+            dice_set.dice[2].value,
+            dice_set.dice[3].value,
+            dice_set.dice[4].value,
         ))
 
 
+class HighRollerGameTests(unittest.TestCase):
+
+    def test_init(self):
+        game = HighRollerGame()
+        self.assertEqual(game.dice_set.get_descriptor(), "5d6")
+        self.assertEqual(game.dice_set.get_total(), 5)
+
+    def test_help(self):
+        game = HighRollerGame()
+        dice = list(game.dice_set.dice)
+        game.run_command("help")
+        self.assertEqual(dice, game.dice_set.dice)
+
+    def test_roll_one(self):
+        game = HighRollerGame()
+        game.run_command("roll", ["2"])
+        self.assertEqual(game.dice_set.get_total(), sum(
+            [die.value for die in game.dice_set.dice]))
+
+    def test_roll_all(self):
+        game = HighRollerGame()
+        game.run_command("roll", ["all"])
+        self.assertEqual(game.dice_set.get_total(), sum(
+            [die.value for die in game.dice_set.dice]))
+
+    def test_use(self):
+        game = HighRollerGame()
+        game.run_command("use", ["20", "1"])
+        self.assertEqual(len(game.dice_set.dice), 1)
+        self.assertEqual(game.dice_set.get_descriptor(), "1d20")
+        for die in game.dice_set.dice:
+            self.assertEqual(die.get_number_sides(), 20)
+
+    def test_unknown_command(self):
+        game = HighRollerGame()
+        dice = list(game.dice_set.dice)
+        game.run_command("unknown")
+        self.assertEqual(dice, game.dice_set.dice)
+
+
 class ExtraCreditTests(unittest.TestCase):
+
     def test_dice_set_matches(self):
-        dice_set_1 = DiceSet(6, 5)
-        dice_set_2 = DiceSet(6, 5)
+        dice_set_1 = DiceSet(6, 5)  # [1, 1, 1, 1, 1], 6 sides each
+        dice_set_2 = DiceSet(6, 5)  # [1, 1, 1, 1, 1], 6 sides each
+        dice_set_3 = DiceSet(5, 5)  # [1, 1, 1, 1, 1], 5 sides each
+        dice_set_4 = DiceSet(6, 4)  # [1, 1, 1, 1], 6 sides each
+
         self.assertTrue(dice_set_1.matches(dice_set_2))
-        dice_set_1.dice = [Die(i, 6) for i in range(1, 6)]
+        self.assertFalse(dice_set_1.matches(dice_set_3))
+        self.assertFalse(dice_set_2.matches(dice_set_4))
+
+        dice_set_1.dice = [Die(i, 6) for i in range(1, 6)]  # [1, 2, 3, 4, 5]
         self.assertFalse(dice_set_1.matches(dice_set_2))
-        dice_set_2.dice = [Die(5 - i, 6) for i in range(5)]
+
+        dice_set_2.dice = list(
+            sorted(dice_set_1.dice, key=lambda _: random.random()))
         self.assertTrue(dice_set_1.matches(dice_set_2))
+
+    def test_quit(self):
+        game = HighRollerGame()
+        with self.assertRaises(SystemExit):
+            game.run_command("quit")
+
+    def test_highest(self):
+        game = HighRollerGame()
+        best_score = game.dice_set.get_total()
+
+        game.run_command("highest")
+        self.assertEqual(game.highest_total, best_score)
+
+        game.run_command("roll", ["all"])
+        best_score = game.dice_set.get_total()
+        self.assertEqual(game.highest_total, best_score)
+
+        game.run_command("use", ["6", "50"])
+        game.run_command("roll", ["all"])
+        best_score = game.dice_set.get_total()
+        self.assertEqual(game.highest_total, best_score)
 
 
 if __name__ == '__main__':
